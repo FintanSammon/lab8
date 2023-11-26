ed4485e347a42d66c8f282456e61ad96f64b6d00
FintanSammon <72512712+FintanSammon@users.noreply.github.com>
2023-11-26 16:38:24 +0000

    Update bookItem.js

--- src/components/bookItem.js
+++ src/components/bookItem.js
@@ -1,30 +1,37 @@
+// Importing the Card component from react-bootstrap for UI styling
 import Card from 'react-bootstrap/Card';
+// Importing Link from react-router-dom for navigation
 import {Link} from 'react-router-dom';
 
+// BookItem component definition
 function BookItem(props) {
-
     return (
         <div>
+            {/* Card component from react-bootstrap to display book details */}
             <Card>
+                {/* Displaying the title of the book in Card's Header */}
                 <Card.Header>{props.myBook.title}</Card.Header>
                 <Card.Body>
+                    {/* Blockquote for styling the content inside the Card's Body */}
                     <blockquote className="blockquote mb-0">
-                        <img src={props.myBook.cover}></img>
+                        {/* Displaying the cover image of the book */}
+                        <img src={props.myBook.cover} alt={props.myBook.title}></img>
+                        {/* Footer to display the author's name */}
                         <footer>
-                            {props.myBook.author
-                            
-                            }
+                            {props.myBook.author}
                         </footer>
                     </blockquote>
                 </Card.Body>
-               <Link to={"/edit/"+props.myBook._id} className='btn btn-primary'>Edit</Link>
+                {/* Link to navigate to the edit page for the book */}
+                <Link to={"/edit/"+props.myBook._id} className='btn btn-primary'>Edit</Link>
             </Card>
+
+            {/* Commented out alternative layout for book item */}
             {/* <h3>{props.myBook.title}</h3>
             <img src={props.myBook.thumbnailUrl}></img>
             <p>{props.myBook.authors[0]}</p> */}
         </div>
     );
-
 }
 
 export default BookItem;
