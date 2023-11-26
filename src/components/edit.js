00c80371b81a49909502dfe2a251993d21c46571
FintanSammon <72512712+FintanSammon@users.noreply.github.com>
2023-11-26 16:42:32 +0000

    Update edit.js

--- src/components/edit.js
+++ src/components/edit.js
@@ -1,81 +1,91 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from "react-router-dom";
+import { useParams } from 'react-router-dom'; // To access URL parameters
+import { useState, useEffect } from 'react'; // Hooks for state and lifecycle management
+import axios from 'axios'; // Axios for making HTTP requests
+import { useNavigate } from "react-router-dom"; // Hook for programmatic navigation
+
 export default function Edit(props) {
-// The useParams hook returns an object of key/value pairs of
-// the dynamic params from the current URL that were matched by
-//the <Route path>.
-let { id } = useParams();
-// update arrays using the React useState()
-// and without the Array objects push() method
-const [title, setTitle] = useState("");
-const [cover, setCover] = useState("");
-const [author, setAuthor] = useState("");
-// useNavigate return a function that we can use to navigate
-const navigate = useNavigate();
-//useEffect Hook is similar componentDidMount
-useEffect(() => {
-    //axios is a promised based web client
-    //make a HTTP Request with GET method and pass as part of the
-    //url.
-    axios.get('http://localhost:4000/api/book/' + id)
-    .then((response) => {
-        // Assign Response data to the arrays using useState.
-setTitle(response.data.title);
-setCover(response.data.cover);
-setAuthor(response.data.author);
-})
-.catch(function (error) {
-console.log(error);
-})
-}, []);
-const handleSubmit = (event) => {
-event.preventDefault();
-const newBook = {
-id: id,
-title: title,
-cover: cover,
-author: author
-};
-axios.put('http://localhost:4000/api/book/' + id, newBook)
-.then((res) => {
-console.log(res.data);
-navigate('/read');
-});
-}
-return (
-<div>
-<form onSubmit={handleSubmit}>
-<div className="form-group">
-<label>Add Book Title: </label>
-<input type="text"
-className="form-control"
-value={title}
-onChange={(e) => setTitle(e.target.value)}
-/>
-</div>
-<div className="form-group">
-<label>Add Release Year: </label>
-<input type="text"
-className="form-control"
-value={cover}
-onChange={(e) => setCover(e.target.value)}
-/>
-</div>
-<div className="form-group">
-<label>Add Poster Url: </label>
-<input type="text"
-className="form-control"
-value={author}
-onChange={(e) => setAuthor(e.target.value)}
-/>
-</div>
-<div className="form-group">
-<input type="submit" value="Edit Book" className="btn btn-primary" />
-</div>
-</form>
-</div>
-);
+    // Retrieves the 'id' parameter from the URL
+    let { id } = useParams();
+
+    // State variables to store book details
+    const [title, setTitle] = useState("");
+    const [cover, setCover] = useState("");
+    const [author, setAuthor] = useState("");
+
+    // Hook to programmatically navigate to other routes
+    const navigate = useNavigate();
+
+    // useEffect to fetch book data when component mounts
+    useEffect(() => {
+        // Fetching book details from the server using Axios
+        axios.get('http://localhost:4000/api/book/' + id)
+            .then((response) => {
+                // Updating state variables with the fetched data
+                setTitle(response.data.title);
+                setCover(response.data.cover);
+                setAuthor(response.data.author);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }, [id]); // Dependency array includes 'id' to re-run useEffect if id changes
+
+    // Handler for form submission
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Constructing new book object from state
+        const newBook = {
+            id: id,
+            title: title,
+            cover: cover,
+            author: author
+        };
+
+        // Sending updated book data to the server
+        axios.put('http://localhost:4000/api/book/' + id, newBook)
+            .then((res) => {
+                console.log(res.data);
+                // Navigate to the 'read' route after successful update
+                navigate('/read');
+            });
+    };
+
+    // JSX for rendering the edit form
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                {/* Input fields for editing book details */}
+                <div className="form-group">
+                    <label>Add Book Title: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Add Release Year: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={cover}
+                        onChange={(e) => setCover(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Add Poster Url: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                    />
+                </div>
+                {/* Submit button */}
+                <div className="form-group">
+                    <input type="submit" value="Edit Book" className="btn btn-primary" />
+                </div>
+            </form>
+        </div>
+    );
 }
